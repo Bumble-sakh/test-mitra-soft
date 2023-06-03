cadbf1440c0087c5a16d33755cd67f958b4aa6a5
Bumble <bumble.sakh@gmail.com>
2023-06-04 10:55:24 +1100

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import ROUTES from './configs/routes';
+import Layout from './components/Layout';
+import Posts from './pages/Posts';
+import About from './pages/About';
+import User from './pages/User';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={ROUTES.index} element={<Layout />}>
+          <Route path={ROUTES.posts} element={<Posts />} />
+          <Route path={ROUTES.about} element={<About />} />
+          <Route path={ROUTES.user} element={<User />} />
+          <Route path={ROUTES.all} element={<Navigate to={ROUTES.index} replace />} />
+          <Route index element={<Posts />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/configs/routes.ts
@@ -0,0 +1,9 @@
+const ROUTES = {
+  index: '/',
+  posts: '/posts',
+  about: '/about',
+  user: '/user',
+  all: '*',
+};
+
+export default ROUTES;
