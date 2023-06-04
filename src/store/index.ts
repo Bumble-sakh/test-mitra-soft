712db2541ec77f584a5070a2cee7410d73208b9a
Bumble <bumble.sakh@gmail.com>
2023-06-04 23:39:41 +1100

    fix: redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-// import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 import { rootSaga } from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 
