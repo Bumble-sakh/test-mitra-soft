e2cce90279ce4e106c280f1e16cef7c79e23a85a
Bumble <bumble.sakh@gmail.com>
2023-06-04 23:40:13 +1100

    feat: add comments

---
+++ src/pages/Posts/Comment/Comment.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { IComment } from '../../../store/comments/types';
+import { Card } from 'react-bootstrap';
+
+type CommentProps = {
+  comment: IComment;
+};
+
+const Comment: FC<CommentProps> = ({ comment }) => {
+  return (
+    <Card>
+      <Card.Header>{comment.email}</Card.Header>
+      <Card.Body>
+        <Card.Text>{comment.body}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Comment;
--- src/pages/Posts/Post/Post.tsx
+++ src/pages/Posts/Post/Post.tsx
@@ -1,15 +1,38 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { IPost } from '../../../store/posts/types';
-import { Button, Card, Image, Nav } from 'react-bootstrap';
+import { Alert, Button, Card, Image, Nav, Spinner, Stack } from 'react-bootstrap';
 import avatar from '../../../assets/icons/avatar.svg';
 import ROUTES from '../../../configs/routes';
 import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import { fetchCommentsRequest } from '../../../store/comments/actions';
+import Comment from '../Comment';
+import { postCommentsState } from '../../../store/comments/types';
 
 type PostProps = {
   post: IPost;
 };
 
+const initialState: postCommentsState = {
+  pending: false,
+  comments: [],
+  error: null,
+};
+
 const Post: FC<PostProps> = ({ post }) => {
+  const postId = post.id;
+  const allComments = useAppSelector((state) => state.comments);
+  const { pending, comments, error } = allComments[postId] || initialState;
+  const dispatch = useAppDispatch();
+
+  const [commentsIsShow, switchCommentsIsShow] = useState(false);
+  const isComments = Boolean(comments?.length);
+
+  const commentsHandler = () => {
+    switchCommentsIsShow((prev) => !prev);
+    !isComments && dispatch(fetchCommentsRequest({ postId }));
+  };
+
   return (
     <Card>
       <Card.Header>
@@ -20,7 +43,30 @@ const Post: FC<PostProps> = ({ post }) => {
       <Card.Body>
         <Card.Title>{post.title} </Card.Title>
         <Card.Text>{post.body}</Card.Text>
-        <Button>Комментарии</Button>
+
+        {pending ? (
+          <Button disabled>
+            <Stack direction='horizontal' gap={2}>
+              <Spinner as='span' animation='border' size='sm' role='status' aria-hidden='true' />
+              <span>Комментарии</span>
+            </Stack>
+          </Button>
+        ) : (
+          <Button onClick={commentsHandler}>Комментарии</Button>
+        )}
+
+        {error && (
+          <Alert variant='danger' className='my-2'>
+            Error: {error}
+          </Alert>
+        )}
+        {!pending && !error && commentsIsShow && (
+          <Stack gap={2} className='my-2'>
+            {comments.map((comment) => (
+              <Comment key={comment.id} comment={comment} />
+            ))}
+          </Stack>
+        )}
       </Card.Body>
     </Card>
   );
