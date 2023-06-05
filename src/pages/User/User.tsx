c4b9abcb4573960812492fcde0a6e3a0ec877e26
Bumble <bumble.sakh@gmail.com>
2023-06-05 14:16:47 +1100

    feat: add user page

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,9 +1,51 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Alert, Spinner, Stack } from 'react-bootstrap';
+import { fetchUserRequest } from '../../store/user/actions';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import Post from '../../components/Post/Post';
+import { fetchUserPostsRequest } from '../../store/userPosts/actions';
+import UserCard from '../../components/UserCard/UserCard';
+import UserCardPlaceholder from '../../components/UserCardPlaceholder/UserCardPlaceholder';
 
 const User = () => {
   const { id } = useParams();
+  const userId = Number(id);
 
-  return <div>Пользователь: {id}</div>;
+  const { pending: pendingUser, user, error: errorUser } = useAppSelector((state) => state.user);
+  const { pending: pendingPosts, posts, error: errorPosts } = useAppSelector((state) => state.userPosts);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchUserRequest({ userId }));
+      dispatch(fetchUserPostsRequest({ userId }));
+    }
+  }, [dispatch, userId]);
+
+  return (
+    <Stack gap={4}>
+      {pendingUser && <UserCardPlaceholder />}
+
+      {errorUser && <Alert variant='danger'>Error: {errorUser}</Alert>}
+
+      {!pendingUser && !errorUser && <UserCard user={user} />}
+
+      {pendingPosts && (
+        <Spinner animation='border' variant='primary' role='status' className='align-self-center'>
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      )}
+
+      {!pendingPosts && !errorPosts && (
+        <Stack gap={2}>
+          {posts?.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </Stack>
+      )}
+    </Stack>
+  );
 };
 
 export default User;
