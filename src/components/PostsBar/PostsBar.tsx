d7f131b88cf5442d6fa7ae21e5c127a6c6bccb14
Bumble <bumble.sakh@gmail.com>
2023-06-06 15:49:17 +1100

    fix: search input

--- src/components/PostsBar/PostsBar.tsx
+++ src/components/PostsBar/PostsBar.tsx
@@ -2,11 +2,12 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 import { Form, Stack, Button, Image } from 'react-bootstrap';
 import sortIcon from '../../assets/icons/sort.svg';
 import searchIcon from '../../assets/icons/search.svg';
-import { useAppDispatch } from '../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { setSearch } from '../../store/displayedPosts/actions';
 
 const PostsBar = () => {
-  const [searchString, setSearchString] = useState('');
+  const { search } = useAppSelector((state) => state.displayedPosts);
+  const [searchString, setSearchString] = useState(search);
   const dispatch = useAppDispatch();
 
   const searchInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -26,6 +27,7 @@ const PostsBar = () => {
           type='search'
           className='me-auto'
           placeholder='Поиск по заголовкам...'
+          defaultValue={searchString}
           onChange={searchInputHandler}
         />
 
